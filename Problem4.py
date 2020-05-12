4c12f1e4a29a806e49323ce4707e60fb3982684f
Divyanshu Jain <55808340+thevyanshu@users.noreply.github.com>
2020-05-13 02:12:43 +0530

    Update Problem4.py

--- Problem4.py
+++ Problem4.py
@@ -1,10 +1,5 @@
 n= 5
 for i in range(n):
-    print("*"*(n-1-i),end="")
-    print("  "*i,end="")
-    if(i<=5):
-        print("*"*(n-1-i))
-for j in range(n):
-    print("*"*j,end="")
-    print("  "*(n-1-j),end="")
-    print("*"*j)
+    print("* " * (n - i) + "    " * i + " *" * (n - i))
+for i in range(2,n + 1):
+    print("* " * i + "    " * (n - i) + " *" * i)
